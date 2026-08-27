039c04b411431a3889b85e526349a62432bcccfc
Oleksandr Yavorskyi <125258821+SvetikUA@users.noreply.github.com>
2026-08-27 20:56:05 +0200

    changed URL

--- src/main.js
+++ src/main.js
@@ -117,7 +117,7 @@ document.getElementById('lang-en').addEventListener('click', () => setLanguage('
 // Form Submission Logic
 const form = document.getElementById('booking-form');
 const formMessage = document.getElementById('form-message');
-const GOOGLE_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby6XzSM1M1VecanXMMpX7U2DCY5_m-QirgBAacCVqk0ffFOoF2uqUBmLqcKdsh3pSge/exec';
+const GOOGLE_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby4ofW0QaclzfkKFXHYsEFdO7AA2sWCCXDiCRAdEsRb59fyrEFIqEKVqXWYcygcr9tI/exec';
 
 if (form) {
   form.addEventListener('submit', async (e) => {
